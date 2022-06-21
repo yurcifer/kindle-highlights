2b4041229af8fe08641f76ad09fef1300f10d92a
yurcifer <yurcifer.dev@gmail.com>
2022-06-21 11:00:03 +0300

    copy to clipbpard function

--- src/components/Highlight/Highlight.jsx
+++ src/components/Highlight/Highlight.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './highlight.module.css'
 import { useStore } from 'react-redux'
 
-export const Highlight = ({title, highlight}) => {
+export const Highlight = ({ title, highlight, author } ) => {
 
   const store = useStore();
+  const copyLabel = 'Copy to clipboard';
+  const copiedLabel = 'Copied';
+  const errorLabel = 'Copied';
+
+  const [copyBtnState, setCopyBtnState] = useState(copyLabel)
+  
 
   const highlightNextEntry = (text, queryLength, index) => {
     return (
@@ -27,12 +33,27 @@ export const Highlight = ({title, highlight}) => {
     return textHighlight(modifiedText, index + query.length + markTextLength);
   }
 
+  const copyToClipboard = () => {
+    const citation = `"${highlight}" - ${title.author}`
+    try {
+      navigator.clipboard.writeText(citation);
+      setCopyBtnState(copiedLabel);
+      setTimeout( () => setCopyBtnState(copyLabel), 2000);
+
+    } catch (e) {
+      console.error('Copy error' + e);
+      setCopyBtnState(errorLabel)
+      setTimeout( () => setCopyBtnState(copyLabel), 3000);
+    }
+  }
+
   return (
     <div className={styles.wrapper} >
       {/* <b>{title.title + ' '}</b><i>({title.author})</i><br/> */}
       <b dangerouslySetInnerHTML={{__html: textHighlight(title.title + ' ')}}></b>
       <i dangerouslySetInnerHTML={{__html: textHighlight(`(${title.author})`)}}></i><br/>
       <div dangerouslySetInnerHTML={{__html: textHighlight(highlight)}} />
+      <button onClick={copyToClipboard}>{copyBtnState}</button>
     </div>
   )
 }
