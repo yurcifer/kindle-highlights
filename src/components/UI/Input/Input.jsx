44a1ab614afa86ece7e3b99f99ac7a4b987b93c6
yurcifer <yurcifer.dev@gmail.com>
2022-06-23 12:42:42 +0300

    eslint fixes

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,10 +1,9 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import styles from './input.module.css';
 
-export const Input = (props) => {
-  return (
-    <div className={styles.wrapper}>
-      <input placeholder="Search here" {...props}></input>
-    </div>
-  )
-}
+export const Input = (props) => (
+  <div className={styles.wrapper}>
+    <input placeholder="Search here" {...props} />
+  </div>
+);
