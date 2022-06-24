ce8c312cc6ccdf242328c0259a8602e56bd8b13a
yurcifer <yurcifer.dev@gmail.com>
2022-06-24 20:22:56 +0300

    style adjustments

--- src/components/DropZone/DropZone.jsx
+++ src/components/DropZone/DropZone.jsx
@@ -20,11 +20,15 @@ export const DropZone = () => {
 
   const onDragLeave = () => {
     counter--;
-    if (counter === 0) wrapperRef.current.classList.remove(styles.dragover);
+    if (counter === 0) {
+      wrapperRef.current.classList.remove(styles.dragover);
+    }
   };
 
   const onDrop = (e = null) => {
-    if (e.target.tagName !== 'INPUT') preventDefaults(e);
+    if (e.target.tagName !== 'INPUT') {
+      preventDefaults(e);
+    }
     wrapperRef.current.classList.remove(styles.dragover);
   };
 
@@ -34,21 +38,23 @@ export const DropZone = () => {
   window.addEventListener('drop', (e) => onDrop(e));
 
   return (
-    <div
-      ref={wrapperRef}
-      className={styles['drop-file-input']}
-      onDragEnter={onDragEnter}
-      onDragLeave={onDragLeave}
-      onDrop={onDrop}
-    >
-      <div className={styles['drop-file-input__label']}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{ fill: 'rgba(0, 0, 0, 1)' }}>
-          <path d="M11 15h2V9h3l-4-5-4 5h3z" />
-          <path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z" />
-        </svg>
-        <p>Drag & Drop "My clippings.txt" here</p>
+    <div className={styles.wrapper}>
+      <div
+        ref={wrapperRef}
+        className={styles['drop-file-input']}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+      >
+        <div className={styles['drop-file-input__label']}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{ fill: 'rgba(0, 0, 0, 1)' }}>
+            <path d="M11 15h2V9h3l-4-5-4 5h3z" />
+            <path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z" />
+          </svg>
+          <p>Drag & Drop "My clippings.txt" here</p>
+        </div>
+        <input type="file" value="" onChange={(e) => uploadFile(e)} />
       </div>
-      <input type="file" value="" onChange={(e) => uploadFile(e)} />
     </div>
   );
 };
