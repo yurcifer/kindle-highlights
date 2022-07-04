649f7aec93cce8d48d897684807fad29f8ca3317
yurcifer <yurcifer.dev@gmail.com>
2022-07-04 16:17:12 +0300

    added client and server folders

--- client/src/routes/About/About.jsx
+++ client/src/routes/About/About.jsx
@@ -10,7 +10,11 @@ export default function About() {
         This site parses &quot;clippings.txt&quot; file that you can find in the root directory of
         your
         {' '}
-        <a target="_blank" href="https://www.amazon.com/s?k=kindle+paperwhite&ref=nb_sb_noss" rel="noreferrer">
+        <a
+          target="_blank"
+          href="https://www.amazon.com/s?k=kindle+paperwhite&ref=nb_sb_noss"
+          rel="noreferrer"
+        >
           Kindle e-reader
         </a>
         . Here you can search through your highlights, copy them and get random ones.
